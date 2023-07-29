e5f004ef435e1d07e320733a2a26ce92049daaab
Vininisu336 <vinicius336@msn.com>
2023-07-29 14:57:13 -0300

    Criado pasta Aula 002 e Comentários.py

---
+++ Aula 002/Comentários.py
@@ -0,0 +1,22 @@
+print("# INÍCIO DO PROGRAMA #")
+print()
+print("Os comentários são linhas de código não processadas no programa")
+print()
+print("Comentários em Python são feitos por # ou por """)
+print()
+print("# são comentários feitos em apenas uma linha - Veja no código")
+# Esta é uma linha de comentário
+
+print()
+print("\"\"\"  são comentários feitos em fárias linhas - Veja no código")
+"""Esta é uma das linhas de comentário
+Esta é a segunda linha de comentário
+Terceira linha de comentário
+Assim por diante - 4
+"""
+print()
+print("Para fechar o comentário de multiplas linhas utiliza-se a mesma sequência de caracteres, as aspas triplas \"\"\"")
+
+print()
+print()
+print("! FIM DO PROGRAMA !")
--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 
 ### Commit 001 - 29.07.2023
 * Criado arquivo Hello World.py
+
+### Commit 002 - 29.07.2023
+* Adicionando Olá mundo! ao arquivo Hello World.py
+* Criando pasta Aula 002
+* Criando o arquivo Comentários.py
